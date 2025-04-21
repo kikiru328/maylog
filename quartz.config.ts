816666188ecf5ebe75343644a9e12325b37ee7a5
kikiru328 <kikiru328@gmail.com>
2025-04-21 16:42:18 +0900

    Quartz sync: Apr 21, 2025, 4:42 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,6 +1,5 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
-import { CustomOgImages } from "./quartz/plugins/emitters/ogImage"
 /**
  * Quartz 4 Configuration
  *
@@ -103,12 +102,7 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.NotFoundPage(),
       // Comment out CustomOgImages to speed up build time
-      Plugin.CustomOgImages({
-        colorScheme: "lightMode",
-        width: 1200,
-        height: 630,
-        excludeRoot: true,
-      }),
+      // Plugin.CustomOgImages(),
     ],
   },
 }
