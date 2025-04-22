20d63cf30891875825033d16662d8a7e338a4266
kikiru328 <kikiru328@gmail.com>
2025-04-22 16:09:47 +0900

    Quartz sync: Apr 22, 2025, 4:09 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#ece2d7",
+          light: "#faf8f8",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
